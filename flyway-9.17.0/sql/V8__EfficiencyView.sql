d8879f91f2f532fe317ecd783f3d08b964de3220
Kevin Chang <kchang@estudiantec.cr>
2023-05-04 23:14:44 -0600

    Cambios Final

--- flyway-9.17.0/sql/V8__EfficiencyView.sql
+++ flyway-9.17.0/sql/V8__EfficiencyView.sql
@@ -4,7 +4,7 @@ set statistics io on;
 select * from Vista;
 
 select * from indexado;
---select * from indexado with (noexpand);
+select * from indexado with (noexpand);
 --CREATE UNIQUE CLUSTERED INDEX IX_Vista ON indexado (responsible_name);
 
 select * from dinamico;
